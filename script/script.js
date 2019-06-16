e556f827291da2a0bd65a1ebd9c0d45211d605db
Devashish Patel <hello@devashishpatel.com>
2019-06-15 20:07:44 -0400

    Added images for the location

--- script/script.js
+++ script/script.js
@@ -3,32 +3,59 @@ const storyData = [
     location: "Ahmedabad",
     title: "Born and brought up in Ahmedabad",
     story:
-      "Ahmedabad, in western India, is the largest city in the state of Gujarat. The Sabarmati River runs through its center. On the western bank is the Gandhi Ashram at Sabarmati, which displays the spiritual leader’s living quarters and artifacts. Across the river, the Calico Museum of Textiles, once a cloth merchant's mansion, has a significant collection of antique and modern fabrics."
+      "Ahmedabad, in western India, is the largest city in the state of Gujarat. The Sabarmati River runs through its center. On the western bank is the Gandhi Ashram at Sabarmati, which displays the spiritual leader’s living quarters and artifacts. Across the river, the Calico Museum of Textiles, once a cloth merchant's mansion, has a significant collection of antique and modern fabrics.",
+    images: [
+      "img/ahmedabad/ahmedabad1.jpg",
+      "img/ahmedabad/ahmedabad2.png",
+      "img/ahmedabad/ahmedabad3.jpg"
+    ]
   },
   {
     location: "Mumbai",
     title: "Last stop before leaving the country",
     story:
-      "Mumbai (formerly called Bombay) is a densely populated city on India’s west coast. A financial center, it's India's largest city. On the Mumbai Harbour waterfront stands the iconic Gateway of India stone arch, built by the British Raj in 1924. Offshore, nearby Elephanta Island holds ancient cave temples dedicated to the Hindu god Shiva. The city's also famous as the heart of the Bollywood film industry."
+      "Mumbai (formerly called Bombay) is a densely populated city on India’s west coast. A financial center, it's India's largest city. On the Mumbai Harbour waterfront stands the iconic Gateway of India stone arch, built by the British Raj in 1924. Offshore, nearby Elephanta Island holds ancient cave temples dedicated to the Hindu god Shiva. The city's also famous as the heart of the Bollywood film industry.",
+
+    images: [
+      "img/mumbai/mumbai1.jpg",
+      "img/mumbai/mumbai2.jpg",
+      "img/mumbai/mumbai3.jpg"
+    ]
   },
   {
     location: "Frankfurt",
     title: "Frist city to visit out of India",
     story:
-      "Frankfurt, a central German city on the river Main, is a major financial hub that's home to the European Central Bank. It's the birthplace of famed writer Johann Wolfgang von Goethe, whose former home is now the Goethe House Museum. Like much of the city, it was damaged during World War II and later rebuilt. The reconstructed Altstadt (Old Town) is the site of Römerberg, a square that hosts an annual Christmas market."
+      "Frankfurt, a central German city on the river Main, is a major financial hub that's home to the European Central Bank. It's the birthplace of famed writer Johann Wolfgang von Goethe, whose former home is now the Goethe House Museum. Like much of the city, it was damaged during World War II and later rebuilt. The reconstructed Altstadt (Old Town) is the site of Römerberg, a square that hosts an annual Christmas market.",
+
+    images: [
+      "img/frankfurt/frankfurt1.jpg",
+      "img/frankfurt/frankfurt2.jpg",
+      "img/frankfurt/frankfurt3.jpg"
+    ]
   },
   {
     location: "Etobicoke",
     title: "Canada's first place of residence",
     story:
-      "Etobicoke is an administrative district and former city that makes up the western part of Toronto, Ontario, Canada. Etobicoke was first settled by Europeans in the 1790s; the municipality grew into city status in the 20th century."
+      "Etobicoke is an administrative district and former city that makes up the western part of Toronto, Ontario, Canada. Etobicoke was first settled by Europeans in the 1790s; the municipality grew into city status in the 20th century.",
+    images: [
+      "img/etobicoke/etobicoke1.jpg",
+      "img/etobicoke/etobicoke2jpg",
+      "img/etobicoke/etobicoke3.jpg"
+    ]
   },
   {
     location: "Brampton",
     title: "Title of story in Brampton",
     story: `Brampton is a city in the Canadian province of Ontario. Situated in Southern Ontario, it is a suburban city in the Greater Toronto Area (GTA) and the seat of Peel Region. The city has a population of 593,638 as of the Canada 2016 Census. Brampton is Canada's ninth-most populous municipality, the seventy-seventh largest city in North America and the third most populous city in the Greater Golden Horseshoe Region, behind Toronto and Mississauga.[2]
 
-    Brampton was incorporated as a village in 1853 with 50 residents, taking its name from the market town of Brampton, in Cumbria, England. In 1873, with 2,000 residents, Brampton was incorporated as a town. The city was once known as "The Flower Town of Canada", a title based on its large greenhouse industry. Today, Brampton's major economic sectors include advanced manufacturing, retail administration, logistics, information, and communication technologies, food and beverage, life sciences, and business services. Mass immigration has greatly increased Brampton's population from 10,000 in the 1950s to over 600,000 today. `
+    Brampton was incorporated as a village in 1853 with 50 residents, taking its name from the market town of Brampton, in Cumbria, England. In 1873, with 2,000 residents, Brampton was incorporated as a town. The city was once known as "The Flower Town of Canada", a title based on its large greenhouse industry. Today, Brampton's major economic sectors include advanced manufacturing, retail administration, logistics, information, and communication technologies, food and beverage, life sciences, and business services. Mass immigration has greatly increased Brampton's population from 10,000 in the 1950s to over 600,000 today. `,
+    images: [
+      "img/brampton/brampton1.png",
+      "img/brampton/brampton2.jpg",
+      "img/brampton/brampton3.jpg"
+    ]
   },
   {
     location: "Winnipeg",
@@ -37,19 +64,34 @@ const storyData = [
 
     The city is named after the nearby Lake Winnipeg; the name comes from the Western Cree words for muddy water. The region was a trading centre for aboriginal peoples long before the arrival of Europeans. French traders built the first fort on the site in 1738. A settlement was later founded by the Selkirk settlers of the Red River Colony in 1812, the nucleus of which was incorporated as the City of Winnipeg in 1873. As of 2011, Winnipeg is the seventh most populated municipality in Canada.[13] Being far inland, the local climate is extremely seasonal even by Canadian standards with average January lows of around −21 °C (−6 °F) and average July highs of 26 °C (79 °F).[7]
     
-    Known as the "Gateway to the West", Winnipeg is a railway and transportation hub with a diversified economy. This multicultural city hosts numerous annual festivals, including the Festival du Voyageur, the Winnipeg Folk Festival, the Jazz Winnipeg Festival, the Winnipeg Fringe Theatre Festival, and Folklorama. Winnipeg was the first Canadian host of the Pan American Games. It is home to several professional sports franchises, including the Winnipeg Blue Bombers (Canadian football), the Winnipeg Jets (ice hockey), Manitoba Moose (ice hockey), Valour FC (soccer), and the Winnipeg Goldeyes (baseball). `
+    Known as the "Gateway to the West", Winnipeg is a railway and transportation hub with a diversified economy. This multicultural city hosts numerous annual festivals, including the Festival du Voyageur, the Winnipeg Folk Festival, the Jazz Winnipeg Festival, the Winnipeg Fringe Theatre Festival, and Folklorama. Winnipeg was the first Canadian host of the Pan American Games. It is home to several professional sports franchises, including the Winnipeg Blue Bombers (Canadian football), the Winnipeg Jets (ice hockey), Manitoba Moose (ice hockey), Valour FC (soccer), and the Winnipeg Goldeyes (baseball). `,
+    images: [
+      "img/winnipeg/winnipeg1.jpg",
+      "img/winnipeg/winnipeg2.jpg",
+      "img/winnipeg/winnipeg3.jpg"
+    ]
   },
   {
     location: "Calgary",
     title: "Dream City",
     story:
-      "Calgary, a cosmopolitan Alberta city with numerous skyscrapers, owes its rapid growth to its status as the centre of Canada’s oil industry. However, it’s still steeped in the western culture that earned it the nickname “Cowtown,” evident in the Calgary Stampede, its massive July rodeo and festival that grew out of the farming exhibitions once presented here."
+      "Calgary, a cosmopolitan Alberta city with numerous skyscrapers, owes its rapid growth to its status as the centre of Canada’s oil industry. However, it’s still steeped in the western culture that earned it the nickname “Cowtown,” evident in the Calgary Stampede, its massive July rodeo and festival that grew out of the farming exhibitions once presented here.",
+    images: [
+      "img/calgary/calgary1.jpg",
+      "img/calgary/calgary2.jpg",
+      "img/calgary/calgary3.jpg"
+    ]
   },
   {
     location: "Chicago",
     title: "Windy City",
     story:
-      "Chicago, on Lake Michigan in Illinois, is among the largest cities in the U.S. Famed for its bold architecture, it has a skyline punctuated by skyscrapers such as the iconic John Hancock Center, 1,451-ft. Willis Tower (formerly the Sears Tower) and the neo-Gothic Tribune Tower. The city is also renowned for its museums, including the Art Institute of Chicago with its noted Impressionist and Post-Impressionist works."
+      "Chicago, on Lake Michigan in Illinois, is among the largest cities in the U.S. Famed for its bold architecture, it has a skyline punctuated by skyscrapers such as the iconic John Hancock Center, 1,451-ft. Willis Tower (formerly the Sears Tower) and the neo-Gothic Tribune Tower. The city is also renowned for its museums, including the Art Institute of Chicago with its noted Impressionist and Post-Impressionist works.",
+    images: [
+      "img/chicago/chicago1.jpg",
+      "img/chicago/chicago2.jpg",
+      "img/chicago/chicago3.jpg"
+    ]
   },
   {
     location: "Parsippany",
@@ -59,13 +101,23 @@ const storyData = [
       Motivated by our faith in Lord Swaminarayan, Yogi Divine Society serves the global community alongside all people, regardless of religion, race, ethnicity, or gender.
       
       Founded in 1974 by its spiritual head His Divine Holiness Guru Hariprasad Swamiji and actively operating in over 500 centers world-wide in countries like India, USA, Canada, Australia, New-Zealand, United Kingdom, Germany, France, South Africa, Kenya.
-      `
+      `,
+    images: [
+      "img/parsippany/parsippany1.jpg",
+      "img/parsippany/parsippany2.jpg",
+      "img/parsippany/parsippany3.jpg"
+    ]
   },
   {
     location: "Mehsana",
     title: "New Family home",
     story:
-      "Mehsana was established by Mehsaji Chavda, Rajput heir of the Chavda dynasty. He constructed the Torana (arc gate) of the city and a temple dedicated to Goddess Toran in Vikram Samvat 1414, Bhadrapad Sud 10 (1358 AD). It is described by Jaisinh Brahmbhatt in poems from 1932 AD, and corroborated by Manilal Nyalchand, an author of Pragat Prabhavi Parshvanath of Samvat 1879 (1822 AD). He also states that Mehsaji built the temple dedicated to Chamunda. It inconclusively establishes that the town was founded during Rajput period. Another legend says that Mehsaji established it in Vikram Samvat 1375 (1319 AD)."
+      "Mehsana was established by Mehsaji Chavda, Rajput heir of the Chavda dynasty. He constructed the Torana (arc gate) of the city and a temple dedicated to Goddess Toran in Vikram Samvat 1414, Bhadrapad Sud 10 (1358 AD). It is described by Jaisinh Brahmbhatt in poems from 1932 AD, and corroborated by Manilal Nyalchand, an author of Pragat Prabhavi Parshvanath of Samvat 1879 (1822 AD). He also states that Mehsaji built the temple dedicated to Chamunda. It inconclusively establishes that the town was founded during Rajput period. Another legend says that Mehsaji established it in Vikram Samvat 1375 (1319 AD).",
+    images: [
+      "img/mehsana/mehsana1.jpg",
+      "img/mehsana/mehsana2.jpg",
+      "img/mehsana/mehsana3.jpg"
+    ]
   }
 ];
 const defaultData = {
@@ -73,7 +125,12 @@ const defaultData = {
   title: "Dummy Title",
   story: `Brampton is a city in the Canadian province of Ontario. Situated in Southern Ontario, it is a suburban city in the Greater Toronto Area (GTA) and the seat of Peel Region. The city has a population of 593,638 as of the Canada 2016 Census. Brampton is Canada's ninth-most populous municipality, the seventy-seventh largest city in North America and the third most populous city in the Greater Golden Horseshoe Region, behind Toronto and Mississauga.[2]
 
-  Brampton was incorporated as a village in 1853 with 50 residents, taking its name from the market town of Brampton, in Cumbria, England. In 1873, with 2,000 residents, Brampton was incorporated as a town. The city was once known as "The Flower Town of Canada", a title based on its large greenhouse industry. Today, Brampton's major economic sectors include advanced manufacturing, retail administration, logistics, information, and communication technologies, food and beverage, life sciences, and business services. Mass immigration has greatly increased Brampton's population from 10,000 in the 1950s to over 600,000 today. `
+  Brampton was incorporated as a village in 1853 with 50 residents, taking its name from the market town of Brampton, in Cumbria, England. In 1873, with 2,000 residents, Brampton was incorporated as a town. The city was once known as "The Flower Town of Canada", a title based on its large greenhouse industry. Today, Brampton's major economic sectors include advanced manufacturing, retail administration, logistics, information, and communication technologies, food and beverage, life sciences, and business services. Mass immigration has greatly increased Brampton's population from 10,000 in the 1950s to over 600,000 today. `,
+  image: {
+    img1: "img/ahmedabad/ahmedabad1.jpg",
+    img2: "img/ahmedabad/ahmedabad1.jpg",
+    img3: "img/ahmedabad/ahmedabad1.jpg"
+  }
 };
 const app = {};
 const apiKeys = {
@@ -124,7 +181,7 @@ app.getData = addressInput => {
 
 app.filter = data => {
   const arr = data.results[0].address_components;
-  //const filtered = arr.filter(arr => arr.types.includes("locality"));
+
   return (cityName = arr.filter(arr => arr.types.includes("locality"))[0]
     .long_name);
 
@@ -161,7 +218,6 @@ app.filter = data => {
 };
 
 // empty all fields before loading the content
-
 app.empty = () => {
   $(".addressTitle").empty();
   $(".story-title").empty();
@@ -169,6 +225,7 @@ app.empty = () => {
   $(".story-title-popup").empty();
   $(".weather-details").empty();
   $(".dateTime-details").empty();
+  $(".composition").empty();
 };
 
 // displaying the data to DOM
@@ -182,6 +239,23 @@ app.displayStoryContent = data => {
     if (cityName.toLowerCase() === stry.location.toLowerCase()) {
       $(".story-title").append(stry.title);
       $(".story-content").append(stry.story);
+      // Displaying Images
+      let imgContent = "";
+      stry.images.forEach((imageLink, index) => {
+        return (imgContent =
+          imgContent +
+          `<img
+          srcset="
+            ${imageLink}  300w,
+            ${imageLink} 1000w
+          "
+          sizes="(max-width: 56.25em) 20vw, (max-width: 37.5em) 30vw, auto"
+          alt="Photo 1"
+          class="composition__photo composition__photo--p${index + 1}"
+          src="${imageLink}"
+        />`);
+      });
+      $(".composition").append(imgContent);
     }
   });
 };
@@ -202,8 +276,9 @@ app.localTime = latLong => {
     }
   }).then(result => {
     // console.log(result);
+    const momentFormated = result.formatted;
     //console.log(`Formatted: ${result.formatted}`);
-    // console.log(`Timestamp: ${result.timestamp}`);
+    //console.log(`moment version: ${moment().format(momentFormated)}`);
 
     const date = new Date(result.formatted);
     // Display Date and Time
